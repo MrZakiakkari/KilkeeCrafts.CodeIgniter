2afcdff97c90d5d45b8484f272fc66ee8034e9c9
M.Zaki Al Akkari <56824972+MrZakiakkari@users.noreply.github.com>
2021-01-08 12:40:46 +0000

    Repository/Service Template

--- application/models/ProductRepository.php
+++ application/models/ProductRepository.php
@@ -1,61 +1,71 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-	class ProductRepository extends CI_Model {
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
-		function __construct() {
-        	//parent::__construct();
-			$this->load->database();
-    	}
-	
-		function insertProductRepository($product) {
-			$this->db->insert('product',$product);
-			return  ($this->db->affected_rows() ==1);
-		}
-		
-		function record_count(){
-			return $this->db->count_all('product');
-		}
-		
-		/*function get_all_product() {
-			$this->db->select("prodCode,FirstName,LastName,YearBorn,Image"); 
-			$this->db->from('product');
-			$query = $this->db->get();
-			return $query->result();
-		}*/
-		function get_all_product($limit, $offset) {
-			$this->db->limit($limit,$offset);
-			$this->db->select("prodCode,prodDescription,prodCategory,prodArtist,prodQtyInStock,prodBuyCost,prodSalePrice,priceAlreadyDiscounted,prodPhoto"); 
-			$this->db->from('product');
-			$query = $this->db->get();
-			return $query->result();
-		}
-		
-		function deleteProductRepository($id) {
-			$this->db->where('prodCode', $id);
-			return $this->db->delete('product');
-		}
-		
-		function updateProductRepository($product,$id) {
-			$this->db->where('prodCode', $id);
-			return $this->db->update('product',$product);
-		}
-		
-		/*WRongly named*/
-		function drilldown($prodCode) {
-			$this->db->select("prodCode,prodDescription,prodCategory,prodArtist,prodQtyInStock,prodBuyCost,prodSalePrice,prodPhoto,priceAlreadyDiscounted");
-			$this->db->from('product');
-			$this->db->where('prodCode',$prodCode);
-			$query = $this->db->get();
-			return $query->result();
-		}
-		function getProductByCode($code) {
+class ProductRepository extends CI_Model {
 
-			$this->db->select("prodCode,prodDescription,prodCategory,prodArtist,prodQtyInStock,prodBuyCost,prodSalePrice,prodPhoto,priceAlreadyDiscounted");
-			$this->db->from('product');
-			$this->db->where('prodCode',$code);
+	protected $table = "Product";
 
-			$query = $this->db->get();
-			return $query->result()[0];
+	function __construct() {
+		//parent::__construct();
+		$this->load->database();
+	}
 
-		}
+	function addProduct($product) {
+		$this->db->insert($this->table, $product);
+		return $this->db->affected_rows() == 1;
+	}
+
+	function deleteProductById($productId) {
+		$this->db->where('Id', $productId);
+		return $this->db->delete($this->table);
+	}
+
+	function updateProduct($product) {
+		$this->db->where('Id',$product->Id);
+		return $this->db->update($this->table, product);
+	}
+
+	function record_count() {
+		return $this->db->count_all('product');
+	}
+
+	/* function get_all_product() {
+	  $this->db->select("prodCode,FirstName,LastName,YearBorn,Image");
+	  $this->db->from('product');
+	  $query = $this->db->get();
+	  return $query->result();
+	  } */
+
+	function get_all_product($limit, $offset) {
+		$this->db->limit($limit, $offset);
+		$this->db->select("prodCode,prodDescription,prodCategory,prodArtist,prodQtyInStock,prodBuyCost,prodSalePrice,priceAlreadyDiscounted,prodPhoto");
+		$this->db->from('product');
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+
+
+	/* WRongly named */
+
+	function drilldown($prodCode) {
+		$this->db->select("prodCode,prodDescription,prodCategory,prodArtist,prodQtyInStock,prodBuyCost,prodSalePrice,prodPhoto,priceAlreadyDiscounted");
+		$this->db->from('product');
+		$this->db->where('prodCode', $prodCode);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	function getProductByCode($code) {
+
+		$this->db->select("prodCode,prodDescription,prodCategory,prodArtist,prodQtyInStock,prodBuyCost,prodSalePrice,prodPhoto,priceAlreadyDiscounted");
+		$this->db->from('product');
+		$this->db->where('prodCode', $code);
+
+		$query = $this->db->get();
+		return $query->result()[0];
+	}
+
 }
