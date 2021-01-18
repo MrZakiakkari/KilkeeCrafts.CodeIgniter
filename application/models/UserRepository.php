3309a47db36aece95812ee89890d59434378b400
M.Zaki Al Akkari <56824972+MrZakiakkari@users.noreply.github.com>
2021-01-18 22:30:54 +0000

    Login User

--- application/models/UserRepository.php
+++ application/models/UserRepository.php
@@ -2,92 +2,99 @@
 
 class UserRepository extends CI_Model
 {
-    public function __construct() 
-    {
-        parent::__construct();
-    }
+
 
     public function getCustomer($email)
     {
         //Checks db for customer with matching email
         $this->db->select('contactFirstName');
-		$this->db->select('customerNumber'); 
-		$this->db->select('password');  
-        $this->db->from('customers');        
+        $this->db->select('customerNumber');
+        $this->db->select('password');
+        $this->db->from('customers');
         $this->db->where('email', $email);
         $query = $this->db->get();
-		
-		//returns users email and password
-        return $query->result_array(); 
+
+        //returns users email and password
+        return $query->result_array();
     }
-	public function addRememberMe($value1,$customerNumber)
+    public function addRememberMe($value1, $customerNumber)
     {
-		// Add users  Remember_me_token
-		//$this->db->query('UPDATE customers SET remember_me_token = "$value1" WHERE customerNumber = "$customerNumber" ');
-		
-		$this->db->set('remember_me_token' , $value1);
-		$this->db->where('customerNumber', $customerNumber);
-		$this->db->update('customers');		
-		$query = $this->db->get() ;
-		return $query->result() ;
+        // Add users  Remember_me_token
+        //$this->db->query('UPDATE customers SET remember_me_token = "$value1" WHERE customerNumber = "$customerNumber" ');
+
+        $this->db->set('remember_me_token', $value1);
+        $this->db->where('customerNumber', $customerNumber);
+        $this->db->update('customers');
+        $query = $this->db->get();
+        return $query->result();
     }
-	
-	public function getRemeberMe()
+
+    public function getRemeberMe()
     {
         $this->db->select('customerNumber,customerName,contactLastName,contactFirstName,phone,addressLine1,addressLine2,city,postalCode,country,creditLimit,email,password,remember_me_token');
-		$this->db->from('customers');
-		$where = "remember_me_token is  NOT NULL";
-		$this->db->where($where);
-		$query = $this->db->get();
-		return $query->result();
+        $this->db->from('customers');
+        $where = "remember_me_token is  NOT NULL";
+        $this->db->where($where);
+        $query = $this->db->get();
+        return $query->result();
     }
-	
-	public function deleteRememberMe($customerNumber)
+
+    public function deleteRememberMe($customerNumber)
     {
-		//$this->db->set('remember_me_token' , null);
-		
-		$data = array( 'remember_me_token' => NULL );
-		//$this->db->where('customerNumber', $customerNumber);
-		//$this->db->update('customers');
-		$this->db->update('customers', $data, 'customerNumber = '.$customerNumber.' ');
-		$query = $this->db->get() ;
-		
-		
-		return $query->result();
+        //$this->db->set('remember_me_token' , null);
+
+        $data = array('remember_me_token' => NULL);
+        //$this->db->where('customerNumber', $customerNumber);
+        //$this->db->update('customers');
+        $this->db->update('customers', $data, 'customerNumber = ' . $customerNumber . ' ');
+        $query = $this->db->get();
+
+
+        return $query->result();
     }
-	
+
     public function getAdmin($username)
     {
-        $this->db->select('adminNumber');  
-        $this->db->select('password');  
+        $this->db->select('adminNumber');
+        $this->db->select('password');
         $this->db->from('admin');
         $this->db->where('username', $username);
         $query = $this->db->get();
-		
-		//returns admins email and password
+
+        //returns admins email and password
         return $query->result_array();
     }
 
     public function createAccount($values)
     {
-        if($this->db->insert('customers', $values))
+        if ($this->db->insert('customers', $values))
             return true;
-        
+
         else
             return false;
     }
 
     public function if_ID_Exists($customerNumber)
     {
-        $this->db->select('customerNumber');  
-        $this->db->from('customers');        
+        $this->db->select('customerNumber');
+        $this->db->from('customers');
         $this->db->where('customerNumber', $customerNumber);
         $query = $this->db->get();
 
-        if(count($query->result_array()) > 0) 
+        if (count($query->result_array()) > 0)
             return true;
 
         else
             return false;
     }
+
+
+    public function GetUserByCredentials($custEmail, $custPassword)
+    {
+        $custPassword= hash($this->passwordhash, $custPassword);
+        $query = $this->db->get_where('customer', array("custEmail" => $custEmail, "custPassword" => $custPassword));
+        return $query->num_rows() == 0 ? null : $query->result()[0];
+        
+    }
+    private $passwordhash= 'sha256';
 }
--- application/views/header.php
+++ application/views/header.php
@@ -1,94 +1,93 @@
 <?php
-	defined('BASEPATH') OR exit('No direct script access allowed');
-	$this->load->helper('url'); 
-	$cssbase = base_url()."assets/css/";
-	$jsbase = base_url()."assets/javascript/";
-	$img_base = base_url()."assets/images/";
-	$base = base_url() . index_page();
+defined('BASEPATH') or exit('No direct script access allowed');
+$this->load->helper('url');
+$cssbase = base_url() . "assets/css/";
+$jsbase = base_url() . "assets/javascript/";
+$img_base = base_url() . "assets/images/";
+$base = base_url() . index_page();
 ?>
 
 <!DOCTYPE>
-<html><head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Books</title>
-<link href="<?php echo $cssbase . "style.css"?>" rel="stylesheet" type="text/css" media="all" />
-<script src="<?php echo $jsbase."common.js"?>"></script>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Books</title>
+	<link href="<?php echo $cssbase . "style.css" ?>" rel="stylesheet" type="text/css" media="all" />
+	<script src="<?php echo $jsbase . "common.js" ?>"></script>
 </head>
 
 <body>
-<header>
-	<img class="center-image" src="<?php echo $img_base . "site/Logo.png"?>" />
+	<header>
+		<img class="center-image" src="<?php echo $img_base . "site/Logo.png" ?>" />
 		<div class="col-5">
-					<form class="navbar-form">
-						<!-- Search -->
-                        <?php
-							echo '<div id="search">';		
-						
-							
-						?>
-							</div>
-					</form>
-                    
-			     </div> 
-				
-				
-				<div class="col-3">
-					<!-- Login -->
-					
-					<div class = "ShoppingBasket">
-					<?php 
-					if($this->session->userdata('customerNumber'))
-					{	
-										
-						//wishlist Size Query	
-						$this->db->select('produceCode');
-						$this->db->from('wishlist');
-						$this->db->where('customerNumber', $this->session->userdata('customerNumber'));
-						$query = $this->db->get();
-						$wishlist_size = count($query->result_array());
-						
-						
-						if($wishlist_size != 0)
-							//Wislist
-							echo '<a href="' . base_url("index.php/ProductController/ProductWishlist")  . '"><img class = "wislistIcon" src=" '.$img_base .'site/wishlist_icon3.png"/><span class = "wishlistIconNumber">(' . $wishlist_size . ')</span></a>';
-						
-						if($this->cart->total_items() != 0)		
-							//Cart
-							echo '<a href="' . base_url("index.php/ProductController/Cart")  . '"><img class = "wislistIcon" src=" '.$img_base .'site/cart_icon.png"/><span>(' .$this->cart->total_items(). ')</span></a>';
-						
-						//Orders
-						echo '<a href="' . base_url("index.php/ProductController/orders")  . '"><span>Orders</span></a>';	
-						//Logout
-						echo '<a href="' . base_url("index.php/UserController/logout_user")  . '"><span>Logout</span></a>';
-					}
-					//Admin
-					else if($this->session->userdata('adminNumber'))
-					{
-						//Control Panel
-						echo '<a href="' . base_url("index.php/UserController/controlPanel") . '"><span>' . $this->session->userdata('username') . '</span></a>';
-						//Admin logout
-						echo '<a href="' . base_url("index.php/UserController/logout_user")  . '"><span>Logout</span></a>';
-						//echo '<div id="'
-					}  
-					else
-					{
-						//Login
-						echo '<div id="header_login">';	
-						echo '<a href="' . base_url() . "index.php/UserController/login" . '">Login/Create Account</a><br>';	
-						echo '</div>';								
-					}
-			
-					?>
-					</div> 
-					<br>
-				</div>
-				
-				
-    <?= anchor('ProductController/index', 'Home', 'title="Home"'); ?>
-	&nbsp;&nbsp;&nbsp;
-	<?= anchor('ProductController/handleInsert', 'Insert', 'title="Insert"'); ?>
-    &nbsp;&nbsp;&nbsp;
-	<?= anchor('ProductController/listproducts/', 'List', 'title="List"'); ?>
-	&nbsp;&nbsp;&nbsp;
+			<form class="navbar-form">
+				<!-- Search -->
+				<?php
+				echo '<div id="search">';
 
-</header>
+
+				?>
+		</div>
+		</form>
+
+		</div>
+
+
+		<div class="col-3">
+			<!-- Login -->
+
+			<div class="ShoppingBasket">
+				<?php
+
+				if ($this->session->userdata('customerNumber')) {
+
+					//wishlist Size Query	
+					$this->db->select('produceCode');
+					$this->db->from('wishlist');
+					$this->db->where('customerNumber', $this->session->userdata('customerNumber'));
+					$query = $this->db->get();
+					$wishlist_size = count($query->result_array());
+
+
+					if ($wishlist_size != 0)
+						//Wislist
+						echo '<a href="' . base_url("index.php/ProductController/ProductWishlist")  . '"><img class = "wislistIcon" src=" ' . $img_base . 'site/wishlist_icon3.png"/><span class = "wishlistIconNumber">(' . $wishlist_size . ')</span></a>';
+
+					if ($this->cart->total_items() != 0)
+						//Cart
+						echo '<a href="' . base_url("index.php/ProductController/Cart")  . '"><img class = "wislistIcon" src=" ' . $img_base . 'site/cart_icon.png"/><span>(' . $this->cart->total_items() . ')</span></a>';
+
+					//Orders
+					echo '<a href="' . base_url("index.php/ProductController/orders")  . '"><span>Orders</span></a>';
+					//Logout
+					echo '<a href="' . base_url("index.php/UserController/logout_user")  . '"><span>Logout</span></a>';
+				}
+				//Admin
+				else if ($this->session->userdata('adminNumber')) {
+					//Control Panel
+					echo '<a href="' . base_url("index.php/UserController/controlPanel") . '"><span>' . $this->session->userdata('username') . '</span></a>';
+					//Admin logout
+					echo '<a href="' . base_url("index.php/UserController/logout_user")  . '"><span>Logout</span></a>';
+					//echo '<div id="'
+				} else {
+					//Login
+					echo '<div id="header_login">';
+					echo '<a href="' . base_url() . "index.php/UserController/login" . '">Login/Create Account</a><br>';
+					echo '</div>';
+				}
+
+				?>
+			</div>
+			<br>
+		</div>
+
+
+		<?= anchor('ProductController/index', 'Home', 'title="Home"'); ?>
+		&nbsp;&nbsp;&nbsp;
+		<?= anchor('ProductController/handleInsert', 'Insert', 'title="Insert"'); ?>
+		&nbsp;&nbsp;&nbsp;
+		<?= anchor('ProductController/listproducts/', 'List', 'title="List"'); ?>
+		&nbsp;&nbsp;&nbsp;
+
+	</header>
