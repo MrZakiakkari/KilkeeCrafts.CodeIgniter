1dab750cc5b49210cb7500367278930c8c69533f
M.Zaki Al Akkari <56824972+MrZakiakkari@users.noreply.github.com>
2021-01-23 16:53:49 +0000

    changes i made to artist and order

--- application/controllers/Artists.php
+++ application/controllers/Artists.php
@@ -1,8 +1,26 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
+class Artists extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('ArtistRepository');
+		$this->load->helper('form');
+		$this->load->helper('html');
+		$this->load->helper('url');
+		$this->load->library('form_validation');
+		$this->load->library('pagination');
+	}
+
+	public function index()
+	{
+		//load the index page
+		$this->load->view('index');
+}
+
+	}
--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -1,8 +1,25 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
+class Orders extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('OrderRepository');
+		$this->load->helper('form');
+		$this->load->helper('html');
+		$this->load->helper('url');
+		$this->load->library('form_validation');
+		$this->load->library('pagination');
+	}
+
+	public function index()
+	{
+		//load the index page
+		$this->load->view('index');
+	}
+}
