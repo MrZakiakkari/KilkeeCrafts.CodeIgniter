ca880823eab568ccf399b3b2027ef91831896434
M.Zaki Al Akkari <56824972+MrZakiakkari@users.noreply.github.com>
2021-02-03 00:04:17 +0000

    Cart checkout

--- application/controllers/ShoppingCart.php
+++ application/controllers/ShoppingCart.php
@@ -19,11 +19,52 @@ class ShoppingCart extends CI_Controller
 
 	public function index()
 	{
-		
-			$this->load->library('cart');
+
+		$this->load->library('cart');
 		$this->load->view('ShoppingCartView');
 	}
-	public function CheckOut(){
-		
+	public function CheckOut()
+	{ {
+			$this->load->library('cart');
+
+			$orderValuesArray = array(
+				"CustomerNumber" => $this->session->userdata("CustomerId"),
+				"date" => now()
+			);
+
+			$order = $this->OrderRepository->addOrder($orderValuesArray);
+			foreach ($this->cart->contents() as $cartItem) {
+				$orderItemValuesArray = array(
+					"OrderNumber" => $order->OrderId,
+					"ProductID" => $cartItem->ProductId,
+					"Date" => now()
+				);
+				$this->OrderRepository->addOrderItem($orderItemValuesArray);
+			}
+
+			$this->cart->clear(); //Something like this
+		}
+	}
+	public function handleAddToCart($productId)
+	{
+		$this->load->library('cart');
+		$cartItem = $this->getCartItemFromProductId($productId);
+		$this->cart->insert($cartItem);
+		$this->load->view('ShoppingCartView');
+	}
+	private function getCartItemFromProductId($productId)
+	{
+		$product  = $this->ProductServices->getProductById($productId);
+		return $this->createCartItem($product->Id, 1, $product->SalePrice, $product->Description);
+	}
+	private function createCartItem($id, $quantity, $price, $name, $options = NULL)
+	{
+		return array(
+			'id'      => $id,
+			'qty'     => $quantity,
+			'price'   => $price,
+			'name'    => $name,
+			'options' => $options
+		);
 	}
 }
